800de0a77501204449cca3b49d3086749b28f3b1
Vadim Zhuravlev <vadim.zhuravlev.qa@gmail.com>
2025-11-08 20:31:44 +0300

    изменены текстовки и добавлены минимальные значения

--- calculationWallpaper.py
+++ calculationWallpaper.py
@@ -1,32 +1,33 @@
 import streamlit as st
 import math
-st.title("Калькулятор для расчета количества обои")
+
+st.title("Калькулятор для расчета количества обоев")
 Swindow = 0
 Sdoor = 0
-lengthRoomSM = st.number_input("Введите длину комнаты в сантиметрах: ")
-widthRoomSM = st.number_input("Введите ширину комнаты в сантиметрах: ")
-heightRoomSM = st.number_input("Введите высоту комнаты в сантиметрах: ")
-lengthRoll = st.number_input("Введите длину рулона обой в метрах: ")
-widthRoll = st.number_input("Введите ширину рулона обой в метрах: ")
+lengthRoomSM = st.number_input("Введите длину комнаты в сантиметрах: ", min_value= 0)
+widthRoomSM = st.number_input("Введите ширину комнаты в сантиметрах: ", min_value= 0)
+heightRoomSM = st.number_input("Введите высоту комнаты в сантиметрах: ", min_value= 0)
+lengthRoll = st.number_input("Введите длину рулона обоев в метрах: ")
+widthRoll = st.number_input("Введите ширину рулона обоев в метрах: ")
 # преобразуем см в м
 lengthRoom = lengthRoomSM / 100
 widthRoom = widthRoomSM / 100
 heightRoom = heightRoomSM / 100
-rapport = st.text_input("Есть ли раппорт (повтор рисунка): Да/Нет: ")
+rapport = st.text_input("Есть ли раппорт (повтор рисунка)? Да/Нет: ")
 if rapport.lower() == 'да':
-    valueRapport = st.number_input("Укажи значение раппорта в метрах: ")
+    valueRapport = st.number_input("Введите значение раппорта в метрах: ")
 window = st.text_input("Есть окна? Да/Нет: ")
 if window.lower() == "да":
-    heightWindowSM = st.number_input("Введите высоту окна в сантиметрах: ")
-    lengthWindowSM = st.number_input("Введите ширину окна в сантиметрах: ")
+    heightWindowSM = st.number_input("Введите высоту окна в сантиметрах: ", min_value= 0)
+    lengthWindowSM = st.number_input("Введите ширину окна в сантиметрах: ", min_value= 0)
     heightWindow = heightWindowSM / 100
     lengthWindow = lengthWindowSM / 100
     Swindow = lengthWindow * heightWindow
     st.write(f'Площадь окна {Swindow} м²')
-door = st.text_input("Есть двери: Да/Нет: ")
+door = st.text_input("Есть двери? Да/Нет: ")
 if door.lower() == "да":
-    heightDoorSM = st.number_input("Введите высоту двери в сантиметрах: ")
-    lengthDoorSM = st.number_input("Введите длину двери в сантиметрах: ")
+    heightDoorSM = st.number_input("Введите высоту двери в сантиметрах: ", min_value= 0)
+    lengthDoorSM = st.number_input("Введите длину двери в сантиметрах: ", min_value= 0)
     heightDoor = heightDoorSM / 100
     lengthDoor = lengthDoorSM / 100
     countDoor = int(st.number_input("Сколько дверей?", min_value= 1, step= 1))
