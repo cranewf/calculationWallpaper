93cb15dbf47bdf061f1e1d270cbe450778b56063
Vadim Zhuravlev <vadim.zhuravlevwf@gmail.com>
2026-01-17 20:39:43 +0300

    - Добавлен выбор единиц измерений раппорта - Добавлена выпадашка

--- calculationWallpaper.py
+++ calculationWallpaper.py
@@ -13,30 +13,36 @@ widthRoll = st.number_input("Введите ширину рулона обоев
 lengthRoom = lengthRoomSM / 100
 widthRoom = widthRoomSM / 100
 heightRoom = heightRoomSM / 100
-rapport = st.text_input("Есть ли раппорт (повтор рисунка)? Да/Нет: ")
+rapport = st.selectbox("Есть ли раппорт (повтор рисунка)?", ["Выберите вариант", "Да", "Нет"])
 if rapport.lower() == 'да':
-    valueRapport = st.number_input("Введите значение раппорта в метрах: ")
-window = st.text_input("Есть окна? Да/Нет: ")
+    measurement = st.selectbox("В каких единицах измерений указан раппорт?", ["Выберите вариант", "Метрах", "Сантиметрах"])
+    if measurement.lower() == 'сантиметрах':
+        valueRapportSM = st.number_input("Введите значение раппорта в сантиметрах:", min_value= 0)
+        valueRapport = valueRapportSM / 100
+    elif measurement.lower() == 'метрах':
+        valueRapportM = st.number_input("Введите значение раппорта в метрах:")
+        valueRapport = valueRapportM
+window = st.selectbox("Есть окна?", ["Выберите вариант", "Да", "Нет"])
 if window.lower() == "да":
     heightWindowSM = st.number_input("Введите высоту окна в сантиметрах: ", min_value= 0)
     lengthWindowSM = st.number_input("Введите ширину окна в сантиметрах: ", min_value= 0)
     heightWindow = heightWindowSM / 100
     lengthWindow = lengthWindowSM / 100
-    Swindow = lengthWindow * heightWindow
-    st.write(f'Площадь окна {Swindow} м²')
-door = st.text_input("Есть двери? Да/Нет: ")
+    Swindow = (lengthWindow * heightWindow)
+    st.write(f'Площадь окна {round(Swindow, 2)} м²')
+door = st.selectbox("Есть двери?", ["Выберите вариант", "Да", "Нет"])
 if door.lower() == "да":
     heightDoorSM = st.number_input("Введите высоту двери в сантиметрах: ", min_value= 0)
-    lengthDoorSM = st.number_input("Введите длину двери в сантиметрах: ", min_value= 0)
+    lengthDoorSM = st.number_input("Введите ширину двери в сантиметрах: ", min_value= 0)
     heightDoor = heightDoorSM / 100
     lengthDoor = lengthDoorSM / 100
     countDoor = int(st.number_input("Сколько дверей?", min_value= 1, step= 1))
     if countDoor > 1:
         Sdoor = (lengthDoor * heightDoor) * countDoor
-        st.write(f'Суммарная площадь всех дверей {Sdoor} м²')
+        st.write(f'Суммарная площадь всех дверей {round(Sdoor, 2)} м²')
     else:
         Sdoor = lengthDoor * heightDoor
-        st.write(f'Площадь двери {Sdoor} м²')
+        st.write(f'Площадь двери {round(Sdoor, 2)} м²')
 if st.button("Рассчитать"):
     # Периметр стен (в метрах)
     P = 2 * (lengthRoom + widthRoom)
@@ -58,5 +64,5 @@ if st.button("Рассчитать"):
         N = math.ceil(S / Sroll)
     st.write(f'Периметр стен {P:.2f} м')
     st.write(f'Площадь стен {S:.2f} м²')
-    st.write(f'Количество рулонов обоев {N} шт.')
     st.write(f'Площадь рулона {Sroll:.2f} м²')
+    st.write(f'Количество рулонов обоев {N} шт.')
